0f8496c9dc93822ed5cc2ea9755f82f360132955
Siddhant <siddhant54.gupta@gmail.com>
2023-06-25 14:22:04 +0530

    Initialize project using Create React App

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'dd8af25f82mshf3da1b93c4b1441p12b8c4jsn2040ad61a4f4',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "29ef318e94ab4fe8648fa03e1a81a2af"
+
